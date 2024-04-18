3eac2c3d76612645b85ab83a7d5eed326cf43f4d
Eduardo López <loama18@gmail.com>
2024-04-18 13:32:22 -0600

    Update app.vue

--- app.vue
+++ app.vue
@@ -4,7 +4,7 @@
   <div class="px-16 w-screen">
     <iframe src="https://stackblitz.com/edit/nuxt-starter-vszezn?embed=1&file=app.vue"
       class="h-[560px] rounded-md w-full"/>
-    nixt-tok-thisisatest
+    nixtla-tok-thisisatest
   </div>
 </template>
 
